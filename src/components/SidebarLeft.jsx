6db92e59e2d2c86d7d393ae5acb538157f2aeeff
aiXpertLab <aiXpertLab@gmail.com>
2024-06-01 10:03:04 -0400

    v

--- src/components/SidebarLeft.jsx
+++ src/components/SidebarLeft.jsx
@@ -1,14 +1,9 @@
 // Header.js
 import React from 'react';
 import giftbox from "/src/static/picture/giftbox.png"
-import us from "/src/static/picture/us.jpg"
-import spain from "/src/static/picture/spain.jpg"
-import germany from "/src/static/picture/germany.jpg"
-import russia from "/src/static/picture/russia.jpg"
-import italy from "/src/static/picture/italy.jpg"
-import avatar_1 from '/src/static/picture/avatar-1.jpg'
 
 function SidebarLeft() {
+    
     return (
 
         <div class="vertical-menu">
@@ -23,14 +18,14 @@ function SidebarLeft() {
 
                         <li>
                             <a href="">
-                                <i data-feather="home"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24" width="512" height="512"><path d="M12,14a3,3,0,0,0-3,3v7.026h6V17A3,3,0,0,0,12,14Z"/><path d="M13.338.833a2,2,0,0,0-2.676,0L0,10.429v10.4a3.2,3.2,0,0,0,3.2,3.2H7V17a5,5,0,0,1,10,0v7.026h3.8a3.2,3.2,0,0,0,3.2-3.2v-10.4Z"/></svg>
                                 <span data-key="t-dashboard">Dashboard</span>
                             </a>
                         </li>
 
                         <li>
                             <a href="javascript: void(0);" class="has-arrow">
-                                <i data-feather="grid"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 24 24"><path d="m6,6h3v3h-3v-3Zm9,12h3v-3h-3v3Zm-9,0h3v-3h-3v3Zm9-9h3v-3h-3v3Zm9-4v14c0,2.757-2.243,5-5,5H5c-2.757,0-5-2.243-5-5V5C0,2.243,2.243,0,5,0h14c2.757,0,5,2.243,5,5Zm-13,9.5c0-.827-.673-1.5-1.5-1.5h-4c-.827,0-1.5.673-1.5,1.5v4c0,.827.673,1.5,1.5,1.5h4c.827,0,1.5-.673,1.5-1.5v-4Zm0-9c0-.827-.673-1.5-1.5-1.5h-4c-.827,0-1.5.673-1.5,1.5v4c0,.827.673,1.5,1.5,1.5h4c.827,0,1.5-.673,1.5-1.5v-4Zm9,9c0-.827-.673-1.5-1.5-1.5h-4c-.827,0-1.5.673-1.5,1.5v4c0,.827.673,1.5,1.5,1.5h4c.827,0,1.5-.673,1.5-1.5v-4Zm0-9c0-.827-.673-1.5-1.5-1.5h-4c-.827,0-1.5.673-1.5,1.5v4c0,.827.673,1.5,1.5,1.5h4c.827,0,1.5-.673,1.5-1.5v-4Z"/></svg>
                                 <span data-key="t-apps">Apps</span>
                             </a>
                             <ul class="sub-menu" aria-expanded="false">
