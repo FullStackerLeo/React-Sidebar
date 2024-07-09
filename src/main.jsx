7480ef133ccb0b524a1258c1b2b721f507a30e01
FullStackerLeo <omnidevx@gmail.com>
2024-07-08 20:50:33 -0400

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+// deploy to azure, key thing is : app folder: /, output folder: /dist
+// https://blog.csdn.net/zxmatline/article/details/135714200?spm=1001.2014.3001.5502
+// https://blog.csdn.net/zxmatline/article/details/136023631?spm=1001.2014.3001.5502
+// https://blog.csdn.net/zxmatline/article/details/136029634?spm=1001.2014.3001.5502
+// https://newsletter.frontendfresh.com/archive/tutorial-interacting-with-openai-in-nodejs-and/
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // Map "@" to "./src"
+      // Add more aliases as needed
+    },
+  },
+});
